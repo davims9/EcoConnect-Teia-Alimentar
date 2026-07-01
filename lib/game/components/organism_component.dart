ac0a492db83ba1ae2211956b0ae42babde77e9e0
Michael Jhonathan <noahsch10833@gmail.com>
2026-07-01 07:58:27 -0300

    Implementado código para animação de sprites em loop

--- lib/game/components/organism_component.dart
+++ lib/game/components/organism_component.dart
@@ -1,15 +1,15 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/effects.dart';
 import 'package:flame/events.dart';
 import '../../core/asset_paths.dart';
 import '../../models/organism.dart';
 import '../food_web_game.dart';
 
-class OrganismComponent extends PositionComponent with DragCallbacks, HasGameRef {
+class OrganismComponent extends SpriteAnimationComponent with DragCallbacks, HasGameRef {
   final Organism organism;
-  Sprite? sprite;
   double _bobPhase = 0;
   double _baseY = 0;
   double _targetGlow = 0;
@@ -25,7 +25,31 @@ class OrganismComponent extends PositionComponent with DragCallbacks, HasGameRef
   Future<void> onLoad() async {
     await super.onLoad();
     final path = OrganismAssetPath.getPath(organism);
-    sprite = await Sprite.load(path);
+    final image = await Flame.images.load(path);
+    
+    final frameWidth = (image.width / 6).floorToDouble();
+    final frameHeight = image.height.toDouble();
+    final textureSize = Vector2(frameWidth, frameHeight);
+    
+    final sprites = [
+      for (int i = 0; i < 6; i++)
+        Sprite(
+          image,
+          srcPosition: Vector2(i * frameWidth, 0),
+          srcSize: textureSize,
+        )
+    ];
+    
+    final pingPongSprites = [
+      ...sprites,
+      for (int i = 4; i > 0; i--) sprites[i],
+    ];
+
+    animation = SpriteAnimation.spriteList(
+      pingPongSprites,
+      stepTime: 0.1,
+      loop: true,
+    );
 
     _baseY = position.y;
     _bobPhase = Random().nextDouble() * 2 * pi;
@@ -107,9 +131,7 @@ class OrganismComponent extends PositionComponent with DragCallbacks, HasGameRef
       canvas.drawCircle(Offset(size.x / 2, size.y / 2), size.x / 2 + 6, glowPaint);
     }
 
-    if (sprite != null) {
-      sprite!.render(canvas, size: size);
-    }
+    super.render(canvas);
 
     final namePainter = TextPainter(
       text: TextSpan(
